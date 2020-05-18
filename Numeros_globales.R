f710b59555fd101f3cca9458e1e42e5774476a26
Federico Stasyszyn <f.stasys@gmail.com>
2020-05-18 14:52:27 -0300

    Estadistica de numeros de recuperados y Notas de fechas faltantes.

--- Numeros_globales.R
+++ Numeros_globales.R
@@ -29,6 +29,7 @@ fallecidos <- function(data) {
   falle <- subset(data, FALLECIDO == "SI")
   TasaDMuerte <- length(falle$fecha_fis) / Ninf
 
+  ###N002: faltan cargar algunas fechas intermedias, repito fis
   F_fis <- falle$fecha_fis
   F_int <- as.Date(falle$FECHA_INTERNACION, format = "%d/%m/%Y")
   w0 <- which(is.na(F_int))
@@ -36,6 +37,7 @@ fallecidos <- function(data) {
   F_cui <- as.Date(falle$FECHA_CUI_INTENSIVOS, format = "%d/%m/%Y")
   w1 <- which(is.na(F_cui))
   F_cui[w1] <- F_int[w1]
+  ###N002/
 
   F_mue <- as.Date(falle$FECHA_FALLECIMIENTO, format = "%d/%m/%Y")
   if (length(which(is.na(F_mue))) != 0) print("MUERTO SIN FECHA!!!!")
@@ -57,6 +59,51 @@ fallecidos <- function(data) {
     Dfis = mean(D_fis), Dcui = mean(D_cui), Dint = mean(D_int),
     Edades = falle$EDAD_ACTUAL,
     D_fis = D_fis, D_cui = D_cui, D_int = D_int,
-    F_mue = F_mue, F_int = F_int, F_cui = F_cui
+    F_mue = F_mue, F_int = F_int, F_cui = F_cui, F_fis = F_fis
+  ))
+}
+
+recuperados <- function(data) {
+  if (missing(data)) data <- prepara_datos("sisa.csv")
+
+
+  Ninf <- length(data$fecha_fis)
+  # ///////////////////
+  ClasSel=grepl("no activo",tolower(data$CLASIFICACION))
+  
+  recu <- subset(data, ClasSel)
+  TasaDRecu <- length(recu$fecha_fis) / Ninf
+
+  ###N002:
+  R_fis <- recu$fecha_fis
+  R_int <- as.Date(recu$FECHA_INTERNACION, format = "%d/%m/%Y")
+  w0 <- which(is.na(R_int))
+  R_int[w0] <- R_fis[w0]
+  R_cui <- as.Date(recu$FECHA_CUI_INTENSIVOS, format = "%d/%m/%Y")
+  w1 <- which(is.na(R_cui))
+  R_cui[w1] <- R_int[w1]
+  #### N003: estan cargados todas las fechas de alta, pero no estan activos. USO FECHA DE ULTIMA ACTUALIZACION
+  R_alta <- as.Date(recu$FECHA_ALTA_MEDICA, format = "%d/%m/%Y")
+  w2 <- which(is.na(R_alta))
+  R_alta[w2] <- as.Date(recu$FECHA_MOD_DIAG[w2], format = "%d/%m/%Y")
+
+  D_int <- as.numeric(R_alta - R_int)
+  D_cui <- as.numeric(R_alta - R_cui)
+  D_fis <- as.numeric(R_alta - R_fis)
+
+
+  print(c("Summario:"))
+  print(c("NRecuperados:", length(D_fis), "Tasa de recuperabilidad (gamma):", TasaDRecu))
+  print("<Tiempos Medios>")
+  print(c("Tiempo desde FIS:", mean(D_fis)))
+  print(c("Tiempo desde Internacion:", mean(D_int)))
+  print(c("Tiempo desde CUI:", mean(D_cui)))
+
+  return(data.frame(
+    Nrec = length(D_fis), TasaDeRecu = TasaDRecu,
+    Dfis = mean(D_fis), Dcui = mean(D_cui), Dint = mean(D_int),
+    Edades = recu$EDAD_ACTUAL,
+    D_fis = D_fis, D_cui = D_cui, D_int = D_int,
+    R_alta = R_alta, R_int = R_int, R_cui = R_cui, R_fis=R_fis
   ))
 }
--- comparo_datos_acumulados.R
+++ comparo_datos_acumulados.R
@@ -1,68 +1,65 @@
- plot_casos <-function(data,...)
- {
-      data_set=subset(data,...)
-      p=acumula_serie(data_set)
+plot_casos <- function(data, ...) {
+  data_set <- subset(data, ...)
+  p <- acumula_serie(data_set)
 
-      if(length(unique(data_set$provincia_carga))==1) {
-        title=paste("Acumulado",data_set$provincia_carga[1])
-      } else {
-	      title="Acumulado Nacional"
-      }
+  if (length(unique(data_set$provincia_carga)) == 1) {
+    title <- paste("Acumulado", data_set$provincia_carga[1])
+  } else {
+    title <- "Acumulado Nacional"
+  }
 
-      ytitle=paste("Número de casos",data_set$clasificacion_resumen[1])
-      xtitle=paste("Dias desde",p[["fecha_min"]])
-      plot(p[["dias"]],p[["cum"]],log="y",type="l",main=title,xlab=xtitle,ylab=ytitle)
- }
+  ytitle <- paste("Número de casos", data_set$clasificacion_resumen[1])
+  xtitle <- paste("Dias desde", p[["fecha_min"]])
+  plot(p[["dias"]], p[["cum"]], log = "y", type = "l", main = title, xlab = xtitle, ylab = ytitle)
+}
 
- oplot_casos <-function(data,col=col,...)
- {
-      data_set=subset(data,...)
-      p=acumula_serie(data_set)
-      lines(p[["dias"]],p[["cum"]],col=col)
- }
- acumula_serie <- function(data)
- {
-       c=as.Date(data$fecha_fis)
-       #extrañamente los datos de la base nacional siempre hay una fecha fis
-       #pero en la base sisa de pilar muchos casos no tiene fis... me parece
-       #que es por los asintomáticos, a nivel nacional algo estan haciendo
-       #por ejemplo estan poniendo la fecha de isopado
-       c=c[!is.na(c)]
-       fecha_min=min(c)
-       c = (as.numeric(c-fecha_min))
-       h=hist(c,breaks=max(c),plot=FALSE)
-       cum=cumsum(h$counts)
-       dias=h$breaks[1:(length(h$breaks)-1)]
- 
-       return(list(dias=dias,cum=cum,fecha_min=fecha_min))
- }
+oplot_casos <- function(data, col = col, ...) {
+  data_set <- subset(data, ...)
+  p <- acumula_serie(data_set)
+  lines(p[["dias"]], p[["cum"]], col = col)
+}
+acumula_serie <- function(data) {
+  c <- as.Date(data$fecha_fis)
+  # extrañamente los datos de la base nacional siempre hay una fecha fis
+  # pero en la base sisa de pilar muchos casos no tiene fis... me parece
+  # que es por los asintomáticos, a nivel nacional algo estan haciendo
+  # por ejemplo estan poniendo la fecha de isopado
+  c <- c[!is.na(c)]
+  fecha_min <- min(c)
+  c <- (as.numeric(c - fecha_min))
+  h <- hist(c, breaks = max(c), plot = FALSE)
+  cum <- cumsum(h$counts)
+  dias <- h$breaks[1:(length(h$breaks) - 1)]
 
- #cargo datos nacionales publicos
- #http://datos.salud.gob.ar/dataset/covid-19-casos-registrados-en-la-republica-argentina
- d_nac <- read.csv("covid19casos.csv")
+  return(list(dias = dias, cum = cum, fecha_min = fecha_min))
+}
 
- #cargo datos provinciales confidenciales
- d_prov <- read.csv("SISA-12-05_pilar.csv")
- #renombro tags para poder usar las mismas expresiones
- colnames(d_prov)[which(colnames(d_prov)=="Fecha1rSintoma")]="fecha_fis"
- colnames(d_prov)[which(colnames(d_prov)=="PROVINCIA_CARGA")]="provincia_carga"
- colnames(d_prov)[which(colnames(d_prov)=="CLASIF_RESUMEN")]="clasificacion_resumen"
- #acomodo el formato de la fecha que no es ISO
- d_prov$fecha_fis=as.Date(d_prov$fecha_fis,format="%d/%m/%Y")
- #a los asintomáticos les asigno la fecha de inicio de sintomas 
- #que esta tambien en otro formato
- for(w in which(is.na(d_prov$fecha_fis)))
- {
-         d_prov$fecha_fis[w]=as.Date(substring(d_prov$Toma_MUESTRA[w],1,10),format="%d-%m-%Y")
- }
+# cargo datos nacionales publicos
+# http://datos.salud.gob.ar/dataset/covid-19-casos-registrados-en-la-republica-argentina
+d_nac <- read.csv("covid19casos.csv")
+
+# cargo datos provinciales confidenciales
+d_prov <- read.csv("SISA-12-05_pilar.csv")
+# renombro tags para poder usar las mismas expresiones
+colnames(d_prov)[which(colnames(d_prov) == "Fecha1rSintoma")] <- "fecha_fis"
+colnames(d_prov)[which(colnames(d_prov) == "PROVINCIA_CARGA")] <- "provincia_carga"
+colnames(d_prov)[which(colnames(d_prov) == "CLASIF_RESUMEN")] <- "clasificacion_resumen"
+# acomodo el formato de la fecha que no es ISO
+d_prov$fecha_fis <- as.Date(d_prov$fecha_fis, format = "%d/%m/%Y")
+# a los asintomáticos les asigno la fecha de inicio de sintomas
+# que esta tambien en otro formato
+for (w in which(is.na(d_prov$fecha_fis)))
+{
+  d_prov$fecha_fis[w] <- as.Date(substring(d_prov$Toma_MUESTRA[w], 1, 10), format = "%d-%m-%Y")
+}
 
 
- #comparo los datos provinciales con los nacionales, esto parece confirmar que en los
- #datos nacionales se carga para los asintomáticos la fecha de inicio de síntomas como
- #la fecha del hisopado
+# comparo los datos provinciales con los nacionales, esto parece confirmar que en los
+# datos nacionales se carga para los asintomáticos la fecha de inicio de síntomas como
+# la fecha del hisopado
 
- pdf("acumulado_confirmados_datos_vs.pdf")
- plot_casos(d_nac,clasificacion_resumen=="Confirmado" & provincia_carga=="Córdoba")
- oplot_casos(d_prov,col="red",clasificacion_resumen=="Confirmado" & provincia_carga=="Córdoba")
- oplot_casos(d_prov,col="blue",clasificacion_resumen=="Confirmado")
- dev.off()
+pdf("acumulado_confirmados_datos_vs.pdf")
+plot_casos(d_nac, clasificacion_resumen == "Confirmado" & provincia_carga == "Córdoba")
+oplot_casos(d_prov, col = "red", clasificacion_resumen == "Confirmado" & provincia_carga == "Córdoba")
+oplot_casos(d_prov, col = "blue", clasificacion_resumen == "Confirmado")
+dev.off()
