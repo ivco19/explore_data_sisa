f6b427ad206eb309be8366821725095b9c56eca9
dpaz2118 <dante.paz@gmail.com>
2020-05-21 15:46:34 -0300

    Nuevo file base madre

--- build_data_series_to_model.R
+++ build_data_series_to_model.R
@@ -1,8 +1,9 @@
  source("funciones.R")
 
- dd=load_data()
- d_nac=dd[["nac"]]
- d_prov=dd[["prov"]]
+ #dd=load_data()
+ #d_nac=dd[["nac"]]
+ #d_prov=dd[["prov"]]
+ d_prov=load_data_prov()
 
  #para la base de datos nacional vamos a ver recuperados usando
  #clasificacion_manual=="Caso confirmado con criterio laboratorial para ALTA"
@@ -11,19 +12,28 @@
  #		clasificacion_resumen=="Confirmado" & 
  #		provincia_carga=="Córdoba")
 
- ss=split_cases(d_prov)
+ #ss=split_cases(d_prov)
+ ss=split_cases_bm(d_prov)
  c_prov=ss[["confirmados"]]
  a_prov=ss[["activos"]]
  r_prov=ss[["recuperados"]]
  f_prov=ss[["fallecidos"]]
  
- c=as.Date(c_prov$fecha_fis)
+ #tfis="fecha_fis"
+ tfis="F_CASO"
+ #trec="FECHA_MOD_DIAG"
+ trec="F_ALTA_M"
+ #tdead=FECHA_FALLECIMIENTO
+ tdead="F_MUERTE"
+
+
+ c=as.Date(c_prov[[tfis]])
  fecha_min=min(c)
- ff=d_prov$fecha_fis
+ ff=d_prov[[tfis]]
  max_fis=max(as.numeric(ff[!is.na(ff)]-fecha_min))
- ff=d_prov$FECHA_MOD_DIAG
+ ff=d_prov[[trec]]
  max_mod=max(as.numeric(ff[!is.na(ff)]-fecha_min))
- ff=d_prov$FECHA_FALLECIMIENTO
+ ff=d_prov[[tdead]]
  max_fall=max(as.numeric(ff[!is.na(ff)]-fecha_min))
 
  maxf=max(c(max_fis,max_mod,max_fall))
@@ -31,25 +41,25 @@
 
  breaks=seq(-0.5,maxf+0.5,by=1)
 
- c_prov=subset(ss[["confirmados"]], EDAD_ACTUAL>=60)
- a_prov=subset(ss[["activos"]], EDAD_ACTUAL>=60)
- r_prov=subset(ss[["recuperados"]], EDAD_ACTUAL>=60)
- f_prov=subset(ss[["fallecidos"]], EDAD_ACTUAL>=60)
+ c_prov=subset(ss[["confirmados"]], EDAD>=60)
+ a_prov=subset(ss[["activos"]], EDAD>=60)
+ r_prov=subset(ss[["recuperados"]], EDAD>=60)
+ f_prov=subset(ss[["fallecidos"]], EDAD>=60)
      
- conf_myrs = acumula_serie(c_prov,"fecha_fis",fecha_min,breaks)
- act_myrs  = acumula_serie(a_prov,"fecha_fis",fecha_min,breaks)
- rec_myrs  = acumula_serie(r_prov,"FECHA_MOD_DIAG",fecha_min,breaks)
- fall_myrs = acumula_serie(f_prov,"FECHA_FALLECIMIENTO",fecha_min,breaks)
+ conf_myrs = acumula_serie(c_prov,tfis,fecha_min,breaks)
+ act_myrs  = acumula_serie(a_prov,tfis,fecha_min,breaks)
+ rec_myrs  = acumula_serie(r_prov,trec,fecha_min,breaks)
+ fall_myrs = acumula_serie(f_prov,tdead,fecha_min,breaks)
 
- c_prov=subset(ss[["confirmados"]], EDAD_ACTUAL<60)
- a_prov=subset(ss[["activos"]], EDAD_ACTUAL<60)
- r_prov=subset(ss[["recuperados"]], EDAD_ACTUAL<60)
- f_prov=subset(ss[["fallecidos"]], EDAD_ACTUAL<60)
+ c_prov=subset(ss[["confirmados"]], EDAD<60)
+ a_prov=subset(ss[["activos"]], EDAD<60)
+ r_prov=subset(ss[["recuperados"]], EDAD<60)
+ f_prov=subset(ss[["fallecidos"]], EDAD<60)
 
- conf_men = acumula_serie(c_prov,"fecha_fis",fecha_min,breaks)
- act_men  = acumula_serie(a_prov,"fecha_fis",fecha_min,breaks)
- rec_men  = acumula_serie(r_prov,"FECHA_MOD_DIAG",fecha_min,breaks)
- fall_men = acumula_serie(f_prov,"FECHA_FALLECIMIENTO",fecha_min,breaks)
+ conf_men = acumula_serie(c_prov,tfis,fecha_min,breaks)
+ act_men  = acumula_serie(a_prov,tfis,fecha_min,breaks)
+ rec_men  = acumula_serie(r_prov,trec,fecha_min,breaks)
+ fall_men = acumula_serie(f_prov,tdead,fecha_min,breaks)
 
 
  to_fit <- data.frame(
@@ -73,7 +83,8 @@
       fallecidos_mayores_acumulados = fall_myrs$cum
  )
 
- write.csv(to_fit,file="data_to_fit.csv",row.names = FALSE)
+ #write.csv(to_fit,file="data_to_fit.csv",row.names = FALSE)
+ write.csv(to_fit,file="data_to_fit_BM.csv",row.names = FALSE)
 
 
 
--- plot_data_series.R
+++ plot_data_series.R
@@ -1,66 +1,75 @@
 source("funciones.R")
 
-dd=load_data()
-d_nac=dd[["nac"]]
-d_prov=dd[["prov"]]
+ #dd=load_data()
+ #d_nac=dd[["nac"]]
+ #d_prov=dd[["prov"]]
+ d_prov=load_data_prov()
 
-#para la base de datos nacional vamos a ver recuperados usando
-#clasificacion_manual=="Caso confirmado con criterio laboratorial para ALTA"
-# rec_nac=subset(d_nac,
-#		clasificacion_manual=="Caso confirmado con criterio laboratorial para ALTA" &
-#		clasificacion_resumen=="Confirmado" & 
-#		provincia_carga=="Córdoba")
+ #para la base de datos nacional vamos a ver recuperados usando
+ #clasificacion_manual=="Caso confirmado con criterio laboratorial para ALTA"
+ # rec_nac=subset(d_nac,
+ #		clasificacion_manual=="Caso confirmado con criterio laboratorial para ALTA" &
+ #		clasificacion_resumen=="Confirmado" & 
+ #		provincia_carga=="Córdoba")
 
- ss=split_cases(d_prov)
-
+ #ss=split_cases(d_prov)
+ ss=split_cases_bm(d_prov)
  c_prov=ss[["confirmados"]]
  a_prov=ss[["activos"]]
  r_prov=ss[["recuperados"]]
  f_prov=ss[["fallecidos"]]
+ 
+ #tfis="fecha_fis"
+ tfis="F_CASO"
+ #trec="FECHA_MOD_DIAG"
+ trec="F_ALTA_M"
+ #tdead=FECHA_FALLECIMIENTO
+ tdead="F_MUERTE"
 
- c=as.Date(c_prov$fecha_fis)
+
+ c=as.Date(c_prov[[tfis]])
  fecha_min=min(c)
- ff=d_prov$fecha_fis
+ ff=d_prov[[tfis]]
  max_fis=max(as.numeric(ff[!is.na(ff)]-fecha_min))
- ff=d_prov$FECHA_MOD_DIAG
+ ff=d_prov[[trec]]
  max_mod=max(as.numeric(ff[!is.na(ff)]-fecha_min))
- ff=d_prov$FECHA_FALLECIMIENTO
+ ff=d_prov[[tdead]]
  max_fall=max(as.numeric(ff[!is.na(ff)]-fecha_min))
 
  maxf=max(c(max_fis,max_mod,max_fall))
  breaks=seq(-0.5,maxf+0.5,by=1)
 
  pdf("casos_covid19_cba.pdf")
-     plot_casos(c_prov,"fecha_fis",fecha_min,breaks,title="Acumulado Córdoba, Pop Gral",ylim=c(1,300))
-     oplot_casos(a_prov,"fecha_fis",fecha_min,breaks,col="red")
-     oplot_casos(r_prov,"FECHA_MOD_DIAG",fecha_min,breaks,col="blue")
-     oplot_casos(f_prov,"FECHA_FALLECIMIENTO",fecha_min,breaks,col="green")
+     plot_casos(c_prov,tfis,fecha_min,breaks,title="Acumulado Córdoba, Pop Gral",ylim=c(1,400))
+     oplot_casos(a_prov,tfis,fecha_min,breaks,col="red")
+     oplot_casos(r_prov,trec,fecha_min,breaks,col="blue")
+     oplot_casos(f_prov,tdead,fecha_min,breaks,col="green")
      
      legend("bottomright",col=c("black","blue","red","green"),
             legend=c("Confirmados","Recuperados","Activos","Fallecidos"),lty=1,bty="n")
 
-     c_prov=subset(ss[["confirmados"]], EDAD_ACTUAL>=60)
-     a_prov=subset(ss[["activos"]], EDAD_ACTUAL>=60)
-     r_prov=subset(ss[["recuperados"]], EDAD_ACTUAL>=60)
-     f_prov=subset(ss[["fallecidos"]], EDAD_ACTUAL>=60)
+     c_prov=subset(ss[["confirmados"]], EDAD>=60)
+     a_prov=subset(ss[["activos"]], EDAD>=60)
+     r_prov=subset(ss[["recuperados"]], EDAD>=60)
+     f_prov=subset(ss[["fallecidos"]], EDAD>=60)
 
-     plot_casos(c_prov,"fecha_fis",fecha_min,breaks,title="Acumulado Córdoba, Mayores de 60",ylim=c(1,300))
-     oplot_casos(a_prov,"fecha_fis",fecha_min,breaks,col="red")
-     oplot_casos(r_prov,"FECHA_MOD_DIAG",fecha_min,breaks,col="blue")
-     oplot_casos(f_prov,"FECHA_FALLECIMIENTO",fecha_min,breaks,col="green")
+     plot_casos(c_prov,tfis,fecha_min,breaks,title="Acumulado Córdoba, Mayores de 60",ylim=c(1,400))
+     oplot_casos(a_prov,tfis,fecha_min,breaks,col="red")
+     oplot_casos(r_prov,trec,fecha_min,breaks,col="blue")
+     oplot_casos(f_prov,tdead,fecha_min,breaks,col="green")
      
      legend("bottomright",col=c("black","blue","red","green"),
             legend=c("Confirmados","Recuperados","Activos","Fallecidos"),lty=1,bty="n")
 
-     c_prov=subset(ss[["confirmados"]], EDAD_ACTUAL<60)
-     a_prov=subset(ss[["activos"]], EDAD_ACTUAL<60)
-     r_prov=subset(ss[["recuperados"]], EDAD_ACTUAL<60)
-     #f_prov=subset(ss[["fallecidos"]], EDAD_ACTUAL<60)
+     c_prov=subset(ss[["confirmados"]], EDAD<60)
+     a_prov=subset(ss[["activos"]], EDAD<60)
+     r_prov=subset(ss[["recuperados"]], EDAD<60)
+     #f_prov=subset(ss[["fallecidos"]], EDAD<60)
 
-     plot_casos(c_prov,"fecha_fis",fecha_min,breaks,title="Acumulado Córdoba, Menores de 60",ylim=c(1,300))
-     oplot_casos(a_prov,"fecha_fis",fecha_min,breaks,col="red")
-     oplot_casos(r_prov,"FECHA_MOD_DIAG",fecha_min,breaks,col="blue")
-     #oplot_casos(f_prov,"FECHA_FALLECIMIENTO",fecha_min,breaks,col="green")
+     plot_casos(c_prov,tfis,fecha_min,breaks,title="Acumulado Córdoba, Menores de 60",ylim=c(1,400))
+     oplot_casos(a_prov,tfis,fecha_min,breaks,col="red")
+     oplot_casos(r_prov,trec,fecha_min,breaks,col="blue")
+     #oplot_casos(f_prov,tdead,fecha_min,breaks,col="green")
      
      legend("bottomright",col=c("black","blue","red","green"),
             legend=c("Confirmados","Recuperados","Activos","Fallecidos"),lty=1,bty="n")
