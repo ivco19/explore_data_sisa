73ebaf4164bea93c44d80a2f9617df553c1d3ce2
dpaz2118 <dante.paz@gmail.com>
2020-05-21 17:07:55 -0300

    Falto un file en el commit anterior

--- funciones.R
+++ funciones.R
@@ -46,6 +46,61 @@
      d_prov$FECHA_FALLECIMIENTO=as.Date(d_prov$FECHA_FALLECIMIENTO,format="%d/%m/%Y")
      return(list(nac=d_nac,prov=d_prov))
  }
+
+ repair_date <-function(char)
+ { 
+	 charo=char
+	 for(i in 1:length(char))
+	 {
+	       	 a=strsplit(char[i],"/")
+		 if(length(a[[1]])>0)
+		 {
+			 if(nchar(a[[1]][3])==2)
+			 {
+			     charo[i]=paste0(paste(a[[1]][1],a[[1]][2],sep="/"),"/2020")
+			 }
+		 }
+	 }
+	 return(charo)
+ }
+
+
+ load_data_prov <-function()
+ {
+     #d_prov <- read.csv("SISA-12-05_pilar.csv")
+     d_prov <- read.csv("BM_2020.05.15.csv")
+     d_prov =subset(d_prov,F_CONF!="")
+     
+     
+     d_prov$FIS=as.Date(repair_date(d_prov$FIS),format="%d/%m/%Y")
+     #a los asintomáticos les asigno la fecha de inicio de sintomas 
+     #que está tambien en otro formato
+     
+     F_CASO=d_prov$FIS
+
+     for(w in which(is.na(d_prov$FIS)))
+     {
+	  ##   print(w)
+	  ##   arr=c( d_prov$PCR_1[w],d_prov$PCR_2[w],
+	  ##          d_prov$PCR_3[w],d_prov$PCR_4[w],
+	  ##          d_prov$PCR_5[w],d_prov$PCR_5[w],
+	  ##          d_prov$PCR_6[w])
+
+	  ##   #que diga no
+	  ##   l1=grepl("no",arr,ignore.case=TRUE)
+          ##   #que diga detectable
+	  ##   l2=grepl("detectable",arr,ignore.case=TRUE)
+          ##   #el primero que diga detectable (sin un no)
+	  ##   pcr_positivo=min(which((!l1)&l2))
+	  ##   tag=paste0("F_PCR_",as.character(pcr_positivo))
+
+             F_CASO[w]=as.Date(repair_date(d_prov$F_CONF[w]),format="%d/%m/%Y")
+     }
+     d_prov$F_CASO=F_CASO
+     d_prov$F_ALTA_M=as.Date(repair_date(d_prov$F_ALTA_M),format="%d/%m/%Y")
+     d_prov$F_MUERTE=as.Date(repair_date(d_prov$F_MUERTE),format="%d/%m/%Y")
+     return(d_prov)
+ }
  split_cases <- function(data)
  {
      clasif=c(
@@ -71,3 +126,12 @@
      return(list(confirmados=confirmados,activos=activos,recuperados=recuperados,fallecidos=fallecidos))
  }
 
+ split_cases_bm <- function(data)
+ {
+     confirmados =data
+     recuperados = subset(data,!is.na(F_ALTA_M))
+     activos     = subset(data,ACTIVO =="SI")
+     fallecidos  = subset(data,!is.na(F_MUERTE))
+
+     return(list(confirmados=confirmados,activos=activos,recuperados=recuperados,fallecidos=fallecidos))
+ }
