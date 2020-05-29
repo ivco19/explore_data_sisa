fdf097fcdb39042f49b01711ae0038fc9a910ad8
Federico Stasyszyn <f.stasys@gmail.com>
2020-05-29 11:43:06 -0300

    Separacion por edades

--- Serie_temp.R
+++ Serie_temp.R
@@ -1,10 +1,38 @@
-do_pl <- function()
+do_pl <- function(sset)
 {
+if(missing(sset)) {
+sset = 0
+print('Recordar args: 0 todos, 1 < 60 años, 2 > 60 años')
+}
+
 source('funciones_NG.R')
 datos=prepara_datos('BM.csv','BM')
 rec=recuperados(datos,verb=FALSE)
 fal=fallecidos(datos,verb=FALSE)
 tod=todos(datos,verb=FALSE)
+##Cuidado esto no esta en las definiciones
+if(sset == 0){
+  alta=rec$alta
+  fmue=fal$mue
+  finf=tod$FInf
+}else if(sset == 1){
+  print("Estadistica de < 60")
+  w=which(rec$Edades < 60)
+  alta=rec$alta[w]
+  w=which(fal$Edades < 60)
+  fmue=fal$mue[w]
+  w=which(tod$EdadInf < 60)
+  finf=tod$FInf[w]
+} else if (sset == 2)
+{
+  print("Estadistica de >= 60")
+  w=which(rec$Edades >= 60)
+  alta=rec$alta[w]
+  w=which(fal$Edades >= 60)
+  fmue=fal$mue[w]
+  w=which(tod$EdadInf >= 60)
+  finf=tod$FInf[w]
+}
 
 f0=min(tod$FInf)
 f1=max(tod$FInf)
@@ -23,18 +51,18 @@ for (i in c(1:Nbins)){
    dates[i]=tbin0+as.integer(Ndias/2)
    print(c(tbin0,tbin1))
    
-   w0=which(rec$alta >= tbin0)
-   w1=which(rec$alta[w0] < tbin1)
+   w0=which(alta >= tbin0)
+   w1=which(alta[w0] < tbin1)
    w=w0[w1] #   print(length(w))
    Nrec[i]=length(w)
    
-   w0=which(fal$mue >= tbin0)
-   w1=which(fal$mue[w0] < tbin1)
+   w0=which(fmue >= tbin0)
+   w1=which(fmue[w0] < tbin1)
    w=w0[w1] #   print(length(w))
    Nmue[i]=length(w)
    
-   w0=which(tod$FInf >= tbin0)
-   w1=which(tod$FInf[w0] < tbin1)
+   w0=which(finf >= tbin0)
+   w1=which(finf[w0] < tbin1)
    w=w0[w1] #   print(length(w))
    Ninf[i]=length(w)
    
@@ -46,8 +74,6 @@ Npob=3.5E6
 gam=crec/cinf
 mu =cmue/cinf
 a=1./5.
-bet=Ninf/Ndias*a
-bet=cinf*Npob/(Npob-cinf-crec-cmue)/dd*a
 bet=cumsum(Ninf)/dd/Ndias
 #print(Nmue)
 #print(cumsum(Nmue))
